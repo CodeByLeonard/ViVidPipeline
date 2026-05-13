41271480f8fe6768c1fea305e422e980070c6071
CodeByLeonard <leonard.freeway116@passinbox.com>
2026-05-14 00:10:48 +0200

    bettered scope panel

--- frontend/src/components/SegmentPanel.tsx
+++ frontend/src/components/SegmentPanel.tsx
@@ -26,141 +26,55 @@ export default function SegmentPanel({
             .padStart(2, "0")}.${ms.toString().padStart(2, "0")}`;
     };
 
+    const container = "" //rounded-2xl border border-dashed border-zinc-700 bg-zinc-900/30
+    const button = "py-2 border border-zinc-700 text-zinc-200 hover:bg-zinc-700/40 transition"
+    const add_segment_button = `${button} px-4 rounded-xl border-dashed bg-zinc-800/50 disabled:opacity-40`
+    const set_button = `${button} px-3 rounded-lg bg-zinc-800/40 text-sm`
+    const time = "flex-1 rounded-lg border border-zinc-800 bg-black/20 px-3 py-2 text-zinc-300 text-sm"
+
     return (
-        <div className="
-            rounded-2xl
-            border border-dashed border-zinc-700
-            bg-zinc-900/30
-            p-5
-        ">
-            {/* HEADER */}
+        <div className={container}>
             <div className="flex items-center justify-between mb-5">
-
                 <div>
-                    <div className="text-zinc-200 font-semibold">
-                        Segments
-                    </div>
-
-                    <div className="text-sm text-zinc-500 mt-1">
-                        Create synchronization anchors
-                    </div>
+                    <div className="text-zinc-200 font-semibold">Segments</div>
+                    <div className="text-sm text-zinc-500 mt-1">Create synchronization anchors</div>
                 </div>
-
-                <button
-                    onClick={addSegment}
-                    disabled={segments.length >= 4}
-                    className="
-                        px-4 py-2 rounded-xl
-                        border border-dashed border-zinc-700
-                        bg-zinc-800/50
-                        text-zinc-200
-                        hover:bg-zinc-700/40
-                        transition
-                        disabled:opacity-40
-                    "
-                >
+                <button onClick={addSegment} disabled={segments.length >= 6} className={add_segment_button}>
                     + Add Segment
                 </button>
-
             </div>
 
-            {/* SEGMENTS - USED TO BE GRID COLS 2 */}
             <div className="grid grid-cols-1 gap-4">
-
                 {segments.map((segment, index) => (
-                    <div
-                        key={segment.id}
-                        className="
-                            rounded-xl
-                            border border-dashed border-zinc-700
-                            bg-black/20
-                            p-4
-                        "
-                    >
-
+                    <div key={segment.id} className="rounded-xl border border-dashed border-zinc-700 bg-black/20 p-4">
                         <div className="text-zinc-300 font-medium mb-4">
                             Segment {index + 1}
                         </div>
 
-                        {/* START */}
-                        <div className="mb-4">
+                        <div className="grid grid-cols-2 gap-4">
 
-                            <div className="text-xs text-zinc-500 mb-2">
-                                Start
+                            {/* START */}
+                            <div>
+                                <div className="text-xs text-zinc-500 mb-2">Start</div>
+                                <div className="flex gap-3">
+                                    <button onClick={() => setStart(segment.id)} className={set_button}>Set Start</button>
+                                    <div className={time}>{formatTime(segment.start)}</div>
+                                </div>
                             </div>
 
-                            <div className="flex gap-3">
-
-                                <button
-                                    onClick={() => setStart(segment.id)}
-                                    className="
-                                        px-3 py-2 rounded-lg
-                                        border border-zinc-700
-                                        bg-zinc-800/40
-                                        text-zinc-200 text-sm
-                                        hover:bg-zinc-700/40
-                                        transition
-                                    "
-                                >
-                                    Set Start
-                                </button>
-
-                                <div className="
-                                    flex-1 rounded-lg
-                                    border border-zinc-800
-                                    bg-black/20
-                                    px-3 py-2
-                                    text-zinc-300 text-sm
-                                ">
-                                    {formatTime(segment.start)}
+                            {/* END */}
+                            <div>
+                                <div className="text-xs text-zinc-500 mb-2">End</div>
+                                <div className="flex gap-3">
+                                    <button onClick={() => setEnd(segment.id)} className={set_button}>Set End</button>
+                                    <div className={time}>{formatTime(segment.end)}</div>
                                 </div>
-
                             </div>
 
                         </div>
-
-                        {/* END */}
-                        <div>
-
-                            <div className="text-xs text-zinc-500 mb-2">
-                                End
-                            </div>
-
-                            <div className="flex gap-3">
-
-                                <button
-                                    onClick={() => setEnd(segment.id)}
-                                    className="
-                                        px-3 py-2 rounded-lg
-                                        border border-zinc-700
-                                        bg-zinc-800/40
-                                        text-zinc-200 text-sm
-                                        hover:bg-zinc-700/40
-                                        transition
-                                    "
-                                >
-                                    Set End
-                                </button>
-
-                                <div className="
-                                    flex-1 rounded-lg
-                                    border border-zinc-800
-                                    bg-black/20
-                                    px-3 py-2
-                                    text-zinc-300 text-sm
-                                ">
-                                    {formatTime(segment.end)}
-                                </div>
-
-                            </div>
-
-                        </div>
-
                     </div>
                 ))}
-
             </div>
-
         </div>
     );
 }
--- frontend/src/pages/ScopePage.tsx
+++ frontend/src/pages/ScopePage.tsx
@@ -13,7 +13,7 @@ export default function ScopePage() {
     const [segments, setSegments] = useState<Segment[]>([]);
 
     const addSegment = () => {
-        if (segments.length >= 4) return;
+        if (segments.length >= 6) return;
         setSegments(prev => [...prev, { id: Date.now(), start: null, end: null }]);
     };
 
@@ -41,6 +41,12 @@ export default function ScopePage() {
         );
     };
 
+    const onFinish = async () => {
+        for (const segment of segments) {
+            await fetch(`http://0.0.0.0:8000/pipeline/scope/${segment.start}`, {method: "POST"})
+        }
+    }
+
     return (
         <div className="w-full h-full flex items-center justify-center p-8">
             <div className="
@@ -58,6 +64,7 @@ export default function ScopePage() {
                     addSegment={addSegment}
                     setStart={setStart}
                     setEnd={setEnd}
+                    onFinish={onFinish}
                 />
             </div>
         </div>
@@ -65,31 +72,25 @@ export default function ScopePage() {
 }
 
 function OriginalMediaSource(
-    {originalVideoRef, segments, addSegment, setStart, setEnd}:
+    {originalVideoRef, segments, addSegment, setStart, setEnd, onFinish}:
     {originalVideoRef: RefObject<HTMLVideoElement | null>, segments: Segment[],
-        addSegment: () => void, setStart: (id: number) => void, setEnd: (id: number) => void }
+        addSegment: () => void, setStart: (id: number) => void, setEnd: (id: number) => void, onFinish: () => void }
 ) {
     return (
         <div className="flex-1 p-6 flex gap-6 overflow-hidden">
-            {/* VIDEO AREA */}
-            <div className="flex-[1.4] min-h-0">
-                <ScopeVideoPanel title="Original Media Source" src="/test/original.mkv" videoRef={originalVideoRef}/>
+            <div className="flex-[1.8] min-h-0">
+                <ScopeVideoPanel title="Original Media Source" src="http://localhost:8000/cache/original.mkv" videoRef={originalVideoRef}/>
             </div>
 
             <div className="flex-1 min-h-0 flex flex-col">
-                {/* SCROLLABLE SEGMENTS */}
-                <div className="flex-1 min-h-0 overflow-y-auto">
+                <div className="flex-1 min-h-0 overflow-y-auto rounded-2xl border border-dashed border-zinc-700 bg-zinc-900/30 p-5">
                     <SegmentPanel segments={segments} addSegment={addSegment} setStart={setStart} setEnd={setEnd}/>
                 </div>
 
-                {/* ACTION BUTTONS */}
                 <div className="mt-4 flex gap-3 pt-4 border-t border-zinc-800">
-                    <button
-                        className="
-                            flex-1 px-4 py-3 rounded-xl
-                            border border-dashed border-cyan-700 bg-cyan-500/10 text-cyan-200 hover:bg-cyan-500/20
-                            transition
-                        "
+                    <button onClick={onFinish} className="
+                            flex-1 px-4 py-3 rounded-xl transition border border-dashed
+                            border-cyan-700 bg-cyan-500/10 text-cyan-200 hover:bg-cyan-500/20"
                     >Finish and Map</button>
                 </div>
             </div>
@@ -100,7 +101,7 @@ function OriginalMediaSource(
 function YouTubeSource() {
     return (
         <div className="w-[25%] border-r border-zinc-800 p-6">
-            <ScopeVideoPanel title="YouTube Reference" src="/test/clip.mp4" compact/>
+            <ScopeVideoPanel title="YouTube Reference" src="http://localhost:8000/cache/clip_Eon2EqOfGbs.mp4" compact/>
         </div>
     )
 }
